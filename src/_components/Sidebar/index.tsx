21e4cbff3eded4e72abef610c4c573fd851254f4
speedlightzx <speedlightbr@gmail.com>
2025-07-11 23:24:01 -0300

    refactor: new side bar

--- src/_components/Sidebar/index.tsx
+++ src/_components/Sidebar/index.tsx
@@ -1,65 +1,77 @@
 "use client"
 
-import { CircleUserRound, ClipboardList, Home, LogOut, Users } from "lucide-react";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton, SidebarHeader, SidebarProvider } from "../ui/sidebar"
+import { CircleUserRound, ClipboardList, LogOut, Users } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
-export default function Sidebar() {
+const items = [
+  {
+    title: "Home",
+    url: "home",
+    icon: Home,
+  },
+  {
+    title: "Chamados",
+    url: "chamados",
+    icon: ClipboardList,
+  },
+  {
+    title: "Funcionários",
+    url: "#",
+    icon: Users,
+  },
+  {
+    title: "Minha conta",
+    url: "#",
+    icon: CircleUserRound,
+  }
+] as const
 
-    type sidebarItem = 'Home' | 'Chamados' | 'Funcionários' | 'Minha conta'
-    const [selectedSidebarItem, setselectedSidebarItem] = useState<sidebarItem>('Home')
+export default function AppSidebar() {
 
-    return (
-        <aside className="h-screen lg:max-w-[25vh] w-full pt-5 pb-5 border-r-2 border-r-indigo-500 justify-between flex flex-col bg-gray-900">
-            
-            <div className="h-[5%]">
-                <h1 className="text-white text-center font-semibold">Nome da empresa</h1>
-            </div>
+        const paths: any = {
+          "home": "Home",
+          "chamados": "Chamados"
+        } as const
 
-            <nav className="flex flex-col justify-center gap-y-13 list-none w-full h-[100%]">
+        const url = usePathname().split("/")[1]
 
-                <Link
-                prefetch
-                href="home"
-                onClick={() => setselectedSidebarItem('Home')}
-                className={`sidebarItem  ${selectedSidebarItem == 'Home' ? 'selectedSidebarItem' : ''}`}>
-                <Home color="white"/>
-                Home
-                </Link>
+        type sidebarItem = 'Home' | 'Chamados' | 'Funcionários' | 'Minha conta'
+        const [selectedSidebarItem, setselectedSidebarItem] = useState<sidebarItem>(paths[url] || "Home")
 
-                <Link
-                prefetch
-                href="chamados"
-                onClick={() => setselectedSidebarItem('Chamados')}
-                className={`sidebarItem  ${selectedSidebarItem == 'Chamados' ? 'selectedSidebarItem' : ''}`}>
-                <ClipboardList color="white"/>
-                Chamados
-                </Link>
-
-                <Link
-                prefetch
-                href="funcionarios"
-                onClick={() => setselectedSidebarItem('Funcionários')}
-                className={`sidebarItem  ${selectedSidebarItem == 'Funcionários' ? 'selectedSidebarItem' : ''}`}>
-                <Users color="white"/>
-                Funcionários
-                </Link>
-
-                <Link
-                prefetch
-                href="minhaconta"
-                onClick={() => setselectedSidebarItem('Minha conta')}
-                className={`sidebarItem  ${selectedSidebarItem == 'Minha conta' ? 'selectedSidebarItem' : ''}`}>
-                <CircleUserRound color="white"/>
-                Minha conta
-                </Link>
-
-            </nav>
-
-            { /* tentar fazer melhor */ }
-            <div className="flex self-center gap-x-2 text-white hover:underline cursor-pointer h-[4%] w-[8.5vh]">
-                <LogOut /> <h1 className="font-semibold">Sair</h1>
-            </div>
-        </aside>
-    )
+  return (
+    <SidebarProvider className="h-screen lg:max-w-[25vh] w-full">
+        <Sidebar className="h-screen lg:max-w-[25vh] w-full border-r-2 border-r-indigo-500">
+            <SidebarHeader className="bg-gray-900">
+                <h1 className="text-center text-white font-bold">Nome da empresa</h1>
+            </SidebarHeader>
+            <SidebarContent className="bg-gray-900">
+                <SidebarGroup>
+                    <SidebarGroupLabel className="text-white">Empresa</SidebarGroupLabel>
+                    <SidebarMenu>
+                        {items.map((item) => {
+                            return (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton className="hover:!bg-indigo-500 font-semibold" asChild>
+                                    <Link
+                                        prefetch
+                                        onClick={() => setselectedSidebarItem(item.title)}
+                                        className={`flex items-center gap-x-1 text-white ${selectedSidebarItem == item.title ? 'selectedSidebarItem' : ''}`}
+                                        href={item.url}>
+                                        {<item.icon />}
+                                        {item.title}
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            )
+                        })}
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
+    </SidebarProvider>
+  )
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,10 +1,17 @@
+import { Metadata } from "next";
 import "../../app/globals.css";
+
+export const metadata: Metadata = {
+  title: "Tech Desk",
+  description: "TechDesk é um gerenciador de aberturas de chamados de TI para empresas."
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body className="bg-neutral-50">
